1710ee49066e4e80515ab3c628d443d5dde87a82
01fe22bca040 <01fe22bca040@kletech.ac.in>
2024-03-10 21:47:04 +0530

    program to display marks

---
+++ student.java
@@ -0,0 +1,19 @@
+public class student {
+    public void stud_det()
+    {
+        String name = "Prajwal";
+        String prg = "BCA";
+        String dept = "MCA";
+        String[] course = {"DevOps", "Machine Learning", "C#"};
+        System.out.println("Name of the student is = "+name);
+        System.out.println("Program of the student is = "+prg);
+        System.out.println("Department of the student is = "+dept);
+        System.out.println("Student registred courses are:");
+        for(int i=0;i<course.length;i++)
+        {
+            System.out.println(course[i]);
+        }
+                
+    }
+    
+} 
